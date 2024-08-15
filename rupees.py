8d6a4364e1f3b13fd73803d20838eb9028eebe16
Alex Kumar <kumaral@oregonstate.edu>
2024-08-15 16:37:07 -0400

    Second Commit

---
+++ rupees.py
@@ -0,0 +1,28 @@
+import time
+import random
+
+while True:
+    time.sleep(3)
+
+    var = open("currency.txt", 'r')
+    var_line = var.readline()
+    var.close()
+
+    if var_line == 'rupees' or var_line == '₹':
+        var = open("converter.txt", 'r')
+        var_line_2 = var.readline()
+        var.close()
+
+        placeh = open('currency.txt', 'w')
+        placeh.write('placeholder')
+        placeh.close()
+
+        new_var = float(var_line_2)
+
+        converted_value = new_var * 83.96
+
+        converted_value_2 = str(converted_value)
+
+        var = open("converter.txt", 'w')
+        var.write(converted_value_2)
+        var.close()
